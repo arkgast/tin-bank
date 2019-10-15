b06dbb3aef4a4d4e7b3f594bec478db26302cd5b
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-10-14 20:04:39 -0400

    feat: add express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world!')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send({ message: 'TIN Bank' })
+})
+
+module.exports = app
