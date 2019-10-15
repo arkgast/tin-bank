c5ed09a7b20dafa9a91da227059e3b834e32195d
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-10-15 10:40:31 -0400

    feat: add offline signing

--- routes/debit.js
+++ routes/debit.js
@@ -2,7 +2,9 @@ const express = require('express')
 const config = require('config')
 const debug = require('debug')('tin-bank:debit')
 const tinapi = require('tinapi_')
+const sdk = require('@webwallet/sdk')
 
+const { KEEPER_PUBLIC, KEEPER_SECRET } = process.env
 const router = express.Router()
 
 const getActionUpload = action => {
@@ -19,12 +21,43 @@ const getActionUpload = action => {
   return createActionRequest
 }
 
+const getClaims = action => {
+  const date = new Date()
+  return {
+    amount: action.amount,
+    domain: 'achtin.minka.io',
+    expiry: date.toISOString(),
+    source: config.get('bank.signerAddress'),
+    symbol: config.get('ach.signerAddress'),
+    target: action.snapshot.target.signer.handle
+  }
+}
+
+const getSigners = () => {
+  return [
+    {
+      secret: KEEPER_SECRET,
+      public: KEEPER_PUBLIC,
+      scheme: 'ecdsa-ed25519',
+      signer: config.get('bank.signerAddress')
+    }
+  ]
+}
+
+const createIOU = action => {
+  const claims = getClaims(action)
+  const signers = getSigners()
+  return sdk.iou.write(claims).sign(signers)
+}
+
 const createAndSignAction = async action => {
   const api = new tinapi.ActionApi()
 
   const actionUpload = getActionUpload(action)
   const actionCreated = await api.createAction(actionUpload)
-  const actionSigned = await api.signAction(actionCreated.action_id)
+
+  const iou = createIOU(actionCreated)
+  const actionSigned = await api.signOffline(actionCreated.action_id, iou)
   debug(actionSigned)
 
   return actionSigned
