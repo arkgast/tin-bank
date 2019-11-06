b67a844719339262bc8dc80000698acc132ba851
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-11-06 18:16:22 -0400

    feat(credit): create and sign action

--- routes/credit.js
+++ routes/credit.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const config = require('config')
 const debug = require('debug')('tin-bank:credit')
+const tinapi = require('tinapi_')
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
+const getActionDownload = action => {
+  const createActionRequest = new tinapi.CreateActionRequest()
+  createActionRequest.source = action.snapshot.target.signer.handle
+  createActionRequest.target = config.get('bank.signerAddress')
+  createActionRequest.amount = action.amount
+  createActionRequest.symbol = action.symbol
+  createActionRequest.labels = {
+    tx_ref: action.labels.tx_ref,
+    type: 'DOWNLOAD'
+  }
+}
+
+const createAndSignAction = async action => {
+  const api = new tinapi.ActionApi()
+
+  const actionDownload = getActionDownload(action)
+  const actionCreated = await api.createAction(actionDownload)
+  const actionSigned = await api.signAction(actionCreated.action_id)
+
+  return actionSigned
+}
+
+router.post('/', async (req, res) => {
   const action = req.body
-  debug(action)
-  res.send(action)
+  const actionSigned = await createAndSignAction(action)
+  debug(actionSigned)
+
+  res.send(actionSigned)
 })
 
 module.exports = router
