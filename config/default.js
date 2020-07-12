00f91bb720cf43a045fa283e212c4b25e7bb81e9
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-07-12 18:04:45 -0400

    refactor: remove unnessesary env variables

--- config/default.js
+++ config/default.js
@@ -1,20 +1,9 @@
-const {
-  URL,
-  BANK_SIGNER,
-  BANK_PUBLIC_KEY,
-  BANK_SECRET_KEY,
-  ACH_SIGNER
-} = process.env
+const { URL, BANK_SIGNER_ADDRESS } = process.env
 
 module.exports = {
   port: 3000,
   projectUrl: URL,
   bank: {
-    signerAddress: BANK_SIGNER,
-    publicKey: BANK_PUBLIC_KEY,
-    secretKey: BANK_SECRET_KEY
-  },
-  ach: {
-    signerAddress: ACH_SIGNER
+    signerAddress: BANK_SIGNER_ADDRESS
   }
 }
